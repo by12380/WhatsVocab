fcc8563d6f4b163af7d603433e2b81c5a9a29cd8
Brian Young <by12380@gmail.com>
2018-05-12 20:31:32 -0700

    Add call to news api and display data

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const DICTIONARY_API_DATA = {
     url: "https://api.pearson.com/v2/dictionaries/ldoce5/entries"
 }
+const NEWS_API_DATA = {
+    newsArticlesUrl: "https://newsapi.org/v2/everything",
+    newsSourcesUrl: "https://newsapi.org/v2/sources",
+    apiKey: "4fc85e70b54a4794a0bc4227b8b1f5c3"
+}
 
 //Need review regaring best practices
 const APP_DATA = {
@@ -13,6 +18,10 @@ function handleFormSubmit() {
         let query = $("#query").val();
         APP_DATA.query = query;
         getDefinitionFromApi(query, displayWordDefinition);
+        getNewsFromApi(query, displayNewsArticles);
+
+        //Clear form
+        $("#query").val("");
     })
 }
 
@@ -37,6 +46,50 @@ function displayWordDefinition (data) {
         }
     }
     $("#dictionaryResults").html(innerHtml);
+}
+
+function getNewsFromApi (word, callback) {
+
+    let newsSourcesData = {
+        language: "en",
+        apiKey: NEWS_API_DATA.apiKey
+    }
+
+    //Retrive news sources data then fetch articles
+    $.getJSON(NEWS_API_DATA.newsSourcesUrl, newsSourcesData, result => {
+        let sources = result.sources.map(source => {
+            return source.id;
+        })
+
+        let data = {
+            q: word,
+            pageSize: 3,
+            sources: sources.join(),
+            apiKey: NEWS_API_DATA.apiKey
+        }
+        $.getJSON(NEWS_API_DATA.newsArticlesUrl, data, callback);
+    })
+    
+}
+
+function displayNewsArticles (data) {
+    let innerHtml = "";
+    for (let article of data.articles) {
+        if (article.description != "") {
+            innerHtml += `
+                <div>
+                    <p>${article.title}</p>
+                    <p>${article.description}</p>
+                    <p>
+                        <a href="${article.url}" target="_blank">
+                            Go to article
+                        </a>
+                    </p>
+                </div>
+            `;
+        }
+    }
+    $("#newsResults").html(innerHtml);
 }
 
 function toFirstCharUpperCase(str) {
